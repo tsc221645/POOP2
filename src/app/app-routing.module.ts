34412c25ded30cc2f6b1ac56b1bbbc16de63bd27
Alejandro <gar231136@uvg.edu.gt>
2023-11-08 01:08:27 -0600

    Diseño Dash-Board

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { DashBoardComponent } from './components/dash-board/dash-board.component';
 
 const routes: Routes = [
   {path: 'index', component: IndexComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'dash-board', component: DashBoardComponent},
+
   { path: '**', component: IndexComponent },
 
 
