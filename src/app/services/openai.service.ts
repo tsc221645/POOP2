4232975370bb7170dc04e8f7e7d937b2884f7c2d
Flaming Penguin <tsc221645@uvg.edu.gt>
2023-11-21 15:43:30 -0600

    open ai services file

---
+++ src/app/services/openai.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { config } from 'src/config1';
+import { Configuration, OpenAIApi } from 'openai';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class OpenaiService {
+  private openai: OpenAIApi;
+  configuration = new Configuration({
+    apiKey: config,
+  });
+  constructor() {
+    this.openai = new OpenAIApi(this.configuration);
+  }
+  generateText(prompt: string):Promise<string | undefined>{
+    return this.openai.createCompletion({
+         model: "text-davinci-003",
+         prompt: prompt,
+         max_tokens: 256
+       }).then(response => {
+         return response.data.choices[0].text;
+       }).catch(error=>{
+         return '';
+       });
+   }
+}
+
